c57be6b1797f49774b4765a35ee995079dd07fea
Safia Abdalla <safia@safia.rocks>
2024-02-13 18:23:11 -0800

    Stash prototype

---
+++ src/Http/Http.Extensions/test/ValidationsGenerator/Initializer.cs
@@ -0,0 +1,8 @@
+using System.Runtime.CompilerServices;
+
+public static class ModuleInitializer
+{
+    [ModuleInitializer]
+    public static void Init() =>
+        VerifySourceGenerators.Initialize();
+}
---
+++ src/Http/Http.Extensions/test/ValidationsGenerator/ValidationsGeneratorTests.ValidatableModels.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Http.Generators.Tests;
+
+public partial class ValidationsGeneratorTests : ValidationsGeneratorTestBase
+{
+    [Fact]
+    public async Task ComplexType()
+    {
+        // Arrange
+        var source = """
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Microsoft.AspNetCore.Validation
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    file sealed class ValidatableModelAttribute : Attribute { }
+}
+
+[Microsoft.AspNetCore.Validation.ValidatableModel]
+public class Model
+{
+    [Range(1, 10)]
+    public int Id { get; set; }
+}
+""";
+        var (driver, updatedCompilation) = await RunGeneratorDriverAsync(source, forModel: true);
+        var diagnostics = updatedCompilation.GetDiagnostics();
+        await Verify(driver.GetRunResult());
+    }
+}
